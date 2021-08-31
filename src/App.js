73b3f0410bb90a0edc8788908b6d23206058b444
Bohdan Vorobel <bohdan.rromanovich@gmail.com>
2021-08-31 21:36:17 +0300

    trying to trhough data handler through a couple components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './app.css';
 import Header from './components/Header/Header';
 import RenderFilter from './components/RenderFilter/RenderFilter';
@@ -6,26 +6,48 @@ import CardContainer from './components/CardContainer/CardContainer';
 
 
 function App() {
-  const [state, setState] = useState([
-    
-  ]);
-  
+  const [state, setState] = useState([]);
+
 
   useEffect(() => {
     fetch('https://rickandmortyapi.com/api/character')
       .then(response => response.json())
       .then(data => data.results.map(item => {
-        return {...item, favourite: false}
+        return {
+          ...item,
+          favourite: false,
+        }
       }))
       .then(data => setState(data))
   }, [])
 
+  const favouriteCountCalc = () => {
+    let result = 0;
+    (state.map(item => {
+      if (item.favourite) {
+        result++;
+      }
+    }));
+    return result
+  }
+  const favouriteNumber = favouriteCountCalc(state);
+
+  const hadleFavourite = (id) => {
+    state.map(item => {
+      if(item.id === id) {
+        item.favourite = !item.favourite;
+      }
+    })
+  }
+
+
+
 
   return (
     <div className="App">
-      <Header favouriteCount='2'/>
+      <Header favouriteCount={favouriteNumber} />
       <RenderFilter />
-      <CardContainer cardsData={state}/>
+      <CardContainer cardsData={state} hadleFavourite={hadleFavourite}/>
     </div>
   );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,10 +3,8 @@ import ButtonDefault from '../Button/Button'
 import './card.css';
 
 
-function CardDefault({ cardsData }) {
-  console.log(cardsData);
-  const { image, name, species, status, favourite } = cardsData;
-
+function CardDefault({ cardsData, hadleFavourite }) {
+  const { image, name, species, status, favourite, id } = cardsData;
 
   return(
     <li className='card-item'>
@@ -21,7 +19,7 @@ function CardDefault({ cardsData }) {
           <p>{status}</p>
         </div>
 
-        {favourite ? <ButtonDefault text='Remove from Favourites' /> : <ButtonDefault text='Add to Favourites' />}
+        {favourite ? <ButtonDefault text='Remove from Favourites' onClick={hadleFavourite} /> : <ButtonDefault text='Add to Favourites' onClick={hadleFavourite}/>}
       </div>
     </li>
   )
